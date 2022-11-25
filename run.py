e1ec649656ceb057529a18f711d2e592cee5ae1b
GaborFicsor <ficsorgabor@gmail.com>
2022-11-25 19:59:03 +0000

    Create board variable that is an array of 3 lists, and create print_board function that prints out the empty board when the game starts

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""
+tic tac toe board
+[
+    [-,-,-],
+    [-,-,-],
+    [-,-,-],
+
+]
+
+possible challenges:
+user want to go first, or let computer go first?
+
+user_input -> specify what the user has to input, user input should be a number between 1 and 9 (1 is the top left spot, and 9 is the right bottom spot)
+
+if they enter anything else: tell them to go again
+check if user_input is already taken
+add user_input to the board
+check if user won: checking rows, columns and diagonals
+toggle between users upon successfull moves
+"""
+
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
+
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(f"{slot} ", end="")
+        print()
+
+print_board(board)
